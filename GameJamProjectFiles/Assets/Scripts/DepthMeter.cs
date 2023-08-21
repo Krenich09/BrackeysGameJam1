afea109c1bf87d93918c5d333ddaae02b03e8ba1
Nadeem Wali <61272669+OrphicNinja@users.noreply.github.com>
2023-08-21 23:04:54 +0100

    Added UI elements

---
+++ GameJamProjectFiles/Assets/Scripts/DepthMeter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using TMPro;
+
+public class DepthMeter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reset numbers to 0
+        //Set controller that contains depth value as variable
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* 
+            Get current distance.
+            Split distance into seperate numbers.
+            Animate number text going up and new number coming from bottom to middle.
+            Update every 2 seconds.
+        */
+    }
+}
