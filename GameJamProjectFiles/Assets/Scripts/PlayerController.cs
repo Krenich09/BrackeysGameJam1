1075c0120794d1dca46d684b108dd62848dd10a2
Krenich09 <108550028+Krenich09@users.noreply.github.com>
2023-08-21 19:34:37 -0400

    AddedDash

--- GameJamProjectFiles/Assets/Scripts/PlayerController.cs
+++ GameJamProjectFiles/Assets/Scripts/PlayerController.cs
@@ -1,24 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Settings")]
     public float rotationSpeed = 5f;  // Speed of rotation
     public float swimForce = 10f;       // Force applied for swimming
     public float offsetAngle = 90f;
-    private Rigidbody2D rb;
-
-    private float rotationSpeedDynamic;
     public LayerMask groundLayer;
-
-
     [Range(0, 1)] public float PosSmoothTime;
+    public float movementSineSpeed = 1;
+    public float movementSineForce = 1;
+
+    [Header("Dash")]
+    public float dashFroce;
+    public float dashDelay;
+    public GameObject dashParticalPrefab;
+    public Transform dashParticalPoint;
+    public KeyCode dashKey;
+
+
+    private Rigidbody2D rb;
+    private float rotationSpeedDynamic;
+    private float sineX;
+    private float startValuemovementSineForce;
     Vector2 refVelocity;
+    private float dashDelayCurrent;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startValuemovementSineForce = movementSineForce;
+        
     }
 
     private void Update()
@@ -28,24 +43,45 @@ public class PlayerController : MonoBehaviour
             if(!Physics2D.OverlapCircle(transform.position + transform.up * 0.3f, 0.3f, groundLayer))
             {
                 movement();
+                dashMovement();
             }
             mouseLook();
+        }        
+    }
+
+    void dashMovement()
+    {
+        // Dash movement
+        dashDelayCurrent -= Time.deltaTime;
+
+        if(Input.GetKeyDown(dashKey) && dashDelayCurrent <= 0)
+        {
+            dashDelayCurrent = dashDelay;
+            Instantiate(dashParticalPrefab, dashParticalPoint.position, quaternion.identity); // Spawn Partical Prefab
+            sineX = 0;
+            movementSineForce *= dashFroce;
         }
+        movementSineForce -= Time.deltaTime;
+        movementSineForce = Mathf.Clamp(movementSineForce, startValuemovementSineForce, movementSineForce);
     }
 
     void movement()
     {
-        Vector2 direction = transform.up * swimForce;
+        float addedY = movementSineForce * Mathf.Sin(movementSineSpeed * sineX) + movementSineForce + 1;
+        sineX += Time.deltaTime;
+
+        Vector2 direction = transform.up * swimForce * addedY;
         Vector2 smoothedPos = Vector2.SmoothDamp(rb.velocity, direction, ref refVelocity, PosSmoothTime);
+        
+        
         rb.velocity = smoothedPos;
     }
     void mouseLook()
     {
-
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = transform.position.z;
 
-        rotationSpeedDynamic = Mathf.Lerp(0.1f, rotationSpeed, Vector2.Distance(transform.position, mousePosition));
+        rotationSpeedDynamic = Mathf.Lerp(0.1f, rotationSpeed, Vector2.Distance(transform.position, mousePosition) / 2);
         Vector3 direction = mousePosition - transform.position;
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + offsetAngle;
@@ -53,7 +89,11 @@ public class PlayerController : MonoBehaviour
         Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
         // Smoothly interpolate towards the target rotation
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeedDynamic * Time.deltaTime);
+
+        if(Vector2.Distance(mousePosition, transform.position) > 1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeedDynamic * Time.deltaTime);
+        }
     }
 
     /// <summary>
--- GameJamProjectFiles/Assets/Scripts/RandomObsticalSpawn.cs
+++ GameJamProjectFiles/Assets/Scripts/RandomObsticalSpawn.cs
@@ -12,7 +12,7 @@ public class RandomObsticalSpawn : MonoBehaviour
     void Update()
     {
 
-        speed = GameObject.Find("speed").GetComponent<speed>().speedValue;
+        /*speed = GameObject.Find("speed").GetComponent<speed>().speedValue;
         int rateAndSpeed = (int)Mathf.Round(rate * (1f / speed));
 
         int chance = UnityEngine.Random.Range(0, rateAndSpeed);
@@ -21,6 +21,6 @@ public class RandomObsticalSpawn : MonoBehaviour
         {
             var position = transform.position + new Vector3(0, UnityEngine.Random.Range(15f, -15f), 0);
             Instantiate(prefab, position, Quaternion.identity);
-        }
+        }*/
     }
 }
