c1cba4bcba0782873950dd7f866faa6750f6b82b
Sunset4242 <129441440+Sunset4242@users.noreply.github.com>
2023-08-23 08:10:11 +0100

    Update RandomObsticalSpawn.cs

--- GameJamProjectFiles/Assets/Scripts/RandomObsticalSpawn.cs
+++ GameJamProjectFiles/Assets/Scripts/RandomObsticalSpawn.cs
@@ -1,26 +1,29 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomObsticalSpawn : MonoBehaviour
 {
     public GameObject prefab;
-    public float rate;
-    public float speed;
-
-
+    public int rate;
+    public float size;
+    public List<GameObject> objects;
+    public List<int> chance2;
     void Update()
     {
-
-        /*speed = GameObject.Find("speed").GetComponent<speed>().speedValue;
-        int rateAndSpeed = (int)Mathf.Round(rate * (1f / speed));
-
-        int chance = UnityEngine.Random.Range(0, rateAndSpeed);
-
-        if (chance == 1 && speed != 0)
+        List<GameObject> objects2 = new List<GameObject>();
+        int chance = UnityEngine.Random.Range(0, rate);
+        if (chance == 1)
         {
-            var position = transform.position + new Vector3(0, UnityEngine.Random.Range(15f, -15f), 0);
-            Instantiate(prefab, position, Quaternion.identity);
-        }*/
+            for (int i = 0; i < objects.Count; i++)
+            {
+                for (int i2 = 0; i2 < chance2[i2]; i2++)
+                {
+                    objects2.Add(objects[i]);
+                }
+            }
+            var position = transform.position + new Vector3(UnityEngine.Random.Range(size, -size), 0, 0);
+            Instantiate(objects2[UnityEngine.Random.Range(0, objects2.Count)], position, Quaternion.identity);
+        }
     }
 }
