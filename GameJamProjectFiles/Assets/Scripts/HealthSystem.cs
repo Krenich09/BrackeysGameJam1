3435e988405cfc0e25787e24566e63c4eaf4cda7
Nadeem Wali <61272669+OrphicNinja@users.noreply.github.com>
2023-08-24 03:29:34 +0100

    Settings panel, Dynamic Heart Icons and start to powerup menu with template

--- GameJamProjectFiles/Assets/Scripts/HealthSystem.cs
+++ GameJamProjectFiles/Assets/Scripts/HealthSystem.cs
@@ -1,21 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour
 {
     public int maxHearts = 3; // Total number of hearts
     private int currentHearts; // Current number of hearts
 
+    public GameObject heartContainer;
+    public GameObject heartIconPrefab;
+    private Image[] heartIcons;
+
     private void Start()
     {
         currentHearts = maxHearts;
+        for (int i = 0; i < maxHearts; i++)
+        {
+            Instantiate(heartIconPrefab, heartContainer.transform);
+            heartIcons = heartContainer.GetComponentsInChildren<Image>();
+        }
+        
     }
 
     public void GetHit()
     {
         if (currentHearts > 0)
         {
+            heartIcons[currentHearts - 1].gameObject.SetActive(false);
             currentHearts--;
 
             if (currentHearts == 0)
